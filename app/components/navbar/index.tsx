61207e1fe706dd506fbdf282ba1a86fcda515faf
ansh3038 <ansh1643.be21@chitkara.edu.in>
2024-02-04 14:06:44 +0530

    Basic authentication added

--- app/components/navbar/index.tsx
+++ app/components/navbar/index.tsx
@@ -1,14 +1,35 @@
+"use client"
 import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function NavBar() {
+  const { data: session } = useSession();
+
   return (
     <>
       <Navbar bg="primary" data-bs-theme="dark">
         <Container>
           <Navbar.Brand href="#home">CodeView</Navbar.Brand>
           <Nav className="ml-auto">
-            <Button variant="primary" className="mr-2">Login</Button>
-            <Button variant="primary">Register</Button>
+            {session ? (
+               <> 
+               {session?.user?.name}
+              <Button variant="primary" onClick={() => signOut()}>
+                Logout
+              </Button>
+              </>
+            ) : (
+              <>
+                <Button
+                  variant="primary"
+                  className="mr-2"
+                  onClick={() => signIn()}
+                >
+                  Login
+                </Button>
+                <Button variant="primary">Register</Button>
+              </>
+            )}
           </Nav>
         </Container>
       </Navbar>
---
+++ app/editor/[...id]/page.tsx
@@ -0,0 +1,5 @@
+export default function EditorPage(){
+    return (<>
+        Hello World
+    </>)
+}
