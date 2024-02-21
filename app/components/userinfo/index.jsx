977e389144729ec0f495df5a03fec7dc42bcec3d
ansh3038 <ansh1643.be21@chitkara.edu.in>
2024-02-21 08:50:13 +0530

    removed avatar bug

--- app/components/userinfo/index.jsx
+++ app/components/userinfo/index.jsx
@@ -12,7 +12,8 @@ import Modal, { ModalProps } from "react-bootstrap/Modal";
 import { Omit, BsPrefixProps } from "react-bootstrap/esm/helpers";
 import { signOut, useSession } from "next-auth/react";
 import Image from "next/image";
-function MyVerticallyCenteredModal( props) {
+import Avatar from "react-avatar";
+function MyVerticallyCenteredModal(props) {
   return (
     <Modal
       {...props}
@@ -26,14 +27,24 @@ function MyVerticallyCenteredModal( props) {
           className="flex justify-between items-center"
         >
           <div className="items-center flex justify-between mx-auto">
-            <div><Image
-              src={props.session?.user?.picture}
-              alt={props.session?.user?.name}
-              width={60}
-              height={60}
-            ></Image>
+            <div className="mr-4"> 
+              {props.session.user.picture ? (
+                <Avatar
+                  githubHandle={props.session.user.name}
+                  size="50px"
+                  round="20px"
+                ></Avatar>
+              ) : (
+                <Avatar
+                  name={props.session.user.name}
+                  size="50px"
+                  round="20px"
+                ></Avatar>
+              )}
             </div>
-            <div className="mx-auto"><h4>{props.session.user.name}</h4></div>
+            <div className="mx-auto">
+              <h4>{props.session.user.name}</h4>
+            </div>
           </div>
         </Modal.Title>
       </Modal.Header>
@@ -53,16 +64,19 @@ function MyVerticallyCenteredModal( props) {
 export default function App() {
   const [modalShow, setModalShow] = useState(false);
   const { data: session } = useSession();
-
+  console.log("session ", session);
   return (
     <>
       <Button variant="primary" onClick={() => setModalShow(true)}>
-        <Image
-          src={session?.user?.picture}
-          alt={session?.user?.name}
-          width={20}
-          height={20}
-        ></Image>
+        {session.user.picture ? (
+          <Avatar
+            githubHandle={session.user.name}
+            size="50px"
+            round="20px"
+          ></Avatar>
+        ) : (
+          <Avatar name={session.user.name} size="50px" round="20px"></Avatar>
+        )}
       </Button>
 
       <MyVerticallyCenteredModal
